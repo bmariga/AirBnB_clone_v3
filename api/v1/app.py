fb997689251fd79a85dfed0c56b4711893a1e8dd
mariga <mariga.bn>
2023-10-28 11:57:13 -0700

    status of your api

---
+++ api/v1/app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""creating a flask application"""
+
+# Import necessary libraries and modules
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from flask_cors import CORS
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Register the blueprint app_views
+app.register_blueprint(app_views)
+
+# Initialize CORS with the app instance, allowing requests from "0.0.0.0"
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+# Declare a method to handle teardown
+@app.teardown_appcontext
+def teardown(exception):
+    """closes the current SQLAlchemy session"""
+    storage.close()
+
+# Define an error handler for 404 Not Found errors
+@app.errorhandler(404)
+def page_not_found(error):
+    """Handler for 404 Not found errors"""
+    return ({'error': 'Not found'}, 404)
+
+# Check if this script is the main entry point
+if __name__ == '__main__':
+    # Retrieve the host and port settings from environment variables
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    # Note: getenv returns a string, and port is converted to an integer
+    # THREADED is set to true so it can serve multiple requests at once
+    app.run(host=host, port=port, threaded=True)
+
