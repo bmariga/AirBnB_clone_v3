d1ac32bf10dc44e8406158342a818c8a5c7ab1e4
mariga <mariga.bn>
2023-10-28 13:39:46 -0700

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python3
-"""Create a view for state"""
+"""Module containing views for State objects"""
 
-from flask import jsonify, request, abort, make_response
 from api.v1.views import app_views
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
-# Route to retrieve the list of all State objects
+# Define routes and actions for State objects
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    """Retrieves the list of all State objects"""
-    states = storage.all(State)
-    return jsonify([obj.to_dict() for obj in states.values()])
+    """
+    Retrieves the list of all State objects
+
+    Returns:
+        A list of dictionaries representing State Objects in JSON format.
+    """
+    states = []
+    for state in storage.all("State").values():
+        states.append(state.to_dict())
+    return jsonify(states)
+
+@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
+def get_state_by_id(state_id=None):
+    """
+    Retrieves a state object by state_id
+
+    Args:
+        state_id (str): The UUID4 string representing a State object.
+
+    Returns:
+        A state object in JSON format.
+        Raise a 404 error if no match for state_id is found.
+    """
+    if state_id:
+        for state in storage.all("State").values():
+            if state.id == state_id:
+                return jsonify(state.to_dict())
+        abort(404)
+
