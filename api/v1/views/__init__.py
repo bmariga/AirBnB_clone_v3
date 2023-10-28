ee6d5db4f4021b46a6a7bdb4b5440a5697be7500
mariga <mariga.bn>
2023-10-28 11:54:06 -0700

    status of your api

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Init defintion for views package"""
+
+# Import the Blueprint class from Flask
+from flask import Blueprint
+
+# Create a Flask Blueprint named "app_views" with a URL prefix of "/api/v1"
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# import of everything in the package api.v1.views.index
+from api.v1.views.index import *
