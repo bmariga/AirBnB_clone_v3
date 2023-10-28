a15555c71523bc57cdab99102b99126ec59a1819
George Emetu <g.emetu11@gmail.com>
2023-10-28 16:27:43 +0100

    air

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,20 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ A method to retrieve one object """
+        if cls in classes.values() and id and type(id) == str:
+            dic_obj = self.all(cls)
+            for key, value in dic_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """ counts """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+
+        return len(data)
